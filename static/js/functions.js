96b9c7600e4df6acd4c8881ab9044963f0a183be
Zoltán Vörös <zvoros@gmail.com>
2013-01-03 20:56:01 +0100

    Initial commit

---
+++ static/js/functions.js
@@ -0,0 +1,307 @@
+// http://stackoverflow.com/questions/4767848/get-caret-cursor-position-in-contenteditable-area-containing-html-content
+// http://stackoverflow.com/questions/3972014/get-caret-position-in-contenteditable-div
+// http://www.allwebdevhelp.com/javascript/js-help-tutorials.php?i=14882
+// http://forums.phpfreaks.com/topic/268622-place-cursor-at-end-of-line-in-editable-div/
+
+var active_div = null
+
+function generate_head_html(id) {
+	var html = ('<div id="div_head_header_' + id + '" class="div_head_header" contenteditable="true" onkeypress="return generic_keypress(event);" onfocus="set_active(this);"></div>' + 
+			'<div id="div_head_body_' + id + '" class="div_head_body"></div>')
+	return html
+}
+
+function generate_plot_html(id) {
+	var html = ('<div id="div_plot_header_' + id + '" class="div_plot_header" contenteditable="true" onkeypress="return generic_keypress(event);" onfocus="set_active(this);"></div>' + 
+				'<div id="div_plot_title_' + id + '" class="div_plot_title"></div>' +
+			'<div id="div_plot_body_' + id + '" class="div_plot_body"></div>')
+	return html
+}
+
+function generate_code_html(id) {
+	var html = ('<div id="div_code_header_' + id + '" class="div_code_header" contenteditable="true" onkeypress="return generic_keypress(event);" onfocus="set_active(this);"></div>' + 
+			'<div id="div_code_body_' + id + '" class="div_code_body"></div>')
+	return html
+}
+
+function generate_text_html(id) {
+	var html = ('<div id="div_text_header_' + id + '" class="div_text_header" contenteditable="true" onkeypress="return generic_keypress(event);" onfocus="set_active(this);"></div>' + 
+			'<div id="div_text_body_' + id + '" class="div_text_body" contenteditable="true" onkeypress="return generic_keypress(event);"></div>')
+	return html
+}
+
+function xml_http_post(url, data, callback) {
+    var req = false;
+    try {
+        // Firefox, Opera 8.0+, Safari
+        req = new XMLHttpRequest();
+    }
+    catch (e) {
+        // Internet Explorer
+        try {
+            req = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch (e) {
+            try {
+                req = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch (e) {
+                alert("Your browser does not support AJAX!");
+                return false;
+            }
+        }
+    }
+    req.open("POST", url, true);
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+            callback(req);
+        }
+    }
+    req.send(data);
+}
+
+function toggle2(showHideDiv, switchTextDiv) {
+	var elem = document.getElementById(showHideDiv);
+	var text = document.getElementById(switchTextDiv);
+	if(elem.style.display == "block") {
+    		elem.style.display = "none";
+		text.innerHTML = "restore";
+  	}
+	else {
+		elem.style.display = "block";
+		text.innerHTML = "collapse";
+	}
+}
+
+function insertAfter(newElement, targetElement) {
+	var parent = targetElement.parentNode;
+	if(parent.lastchild == targetElement) {
+		parent.appendChild(newElement)
+	} else {
+		parent.insertBefore(newElement, targetElement.nextSibling)
+	}
+}
+
+function addKeyPressListener(divide, listener) {
+	if (typeof divide.addEventListener != "undefined") {
+		divide.addEventListener("keypress", listener, false);
+	} else if (typeof divide.attachEvent != "undefined") {
+		divide.attachEvent("onkeypress", listener);
+	}
+}
+
+function get_index(obj) {
+	var num = obj.split("_")
+	return parseInt(num[num.length-1])
+}
+
+function get_num(divide) {
+	var num = divide.id.split("_")
+	return parseInt(num[num.length-1])
+}
+
+function get_max_index(className) {
+	var elems = document.getElementsByClassName(className)
+	console.log(elems.length)
+	var num = 0
+	for(i=0; i < elems.length; i++) {
+		if(num < get_num(elems[i])) {
+			num = get_num(elems[i])
+		}
+	}
+	return num
+}
+
+function create_and_insert(className, position) {
+	if(active_div) position = active_div.id.replace('_header_', '_main_')
+
+	var num = get_max_index(className) + 1
+	var new_div = document.createElement("div")
+	new_div.setAttribute("class", className)
+	new_div.id = className + '_' + num
+	if (className == "div_head_main") {
+		new_div.innerHTML = generate_head_html(num)
+	} else if (className == "div_plot_main") {
+		new_div.innerHTML = generate_plot_html(num)
+	} else if (className == "div_text_main") {
+		new_div.innerHTML = generate_text_html(num)
+	} else if (className == "div_code_main") {
+		new_div.innerHTML = generate_code_html(num)
+	}
+	insertAfter(new_div, document.getElementById(position))
+	document.getElementById(className.replace('_main', '_header_'+num)).focus()
+}
+
+function generic_keypress(event) {
+	console.log(event.target.id)
+	if (event.which === 13 && event.ctrlKey) {
+		if(event.target.id.substring(0, 16) == "div_head_header_") {
+			head_data(event.target)
+		} else if(event.target.id.substring(0, 16) == "div_plot_header_") {
+			plot_data(event.target)
+		} else if(event.target.id.substring(0, 16) == "div_code_header_") {
+			code_data(event.target)
+		} else if(event.target.id.substring(0, 16) == "div_text_header_") {
+			text_data(event.target)
+		} else if(event.target.id.substring(0, 14) == "div_text_body_") {
+			evaluate_text(event.target)
+		}
+		return false
+	}
+	if (event.which === 13 && event.shiftKey) {
+		if(event.target.id.substring(0, 16) == "div_head_header_") {
+		}
+		return false
+	}
+	return true
+}
+
+function create_message(div_data, message_type) {
+	var message = new Object()
+	message.type = message_type
+	message.id = div_data.id
+	message.content = div_data.innerHTML
+	return message
+}
+
+function evaluate_text(div_data) {
+	var math = div_data.id
+	MathJax.Hub.Queue(["Typeset", MathJax.Hub, math]);
+	//var message = create_message(div_data, "texteval")
+    //xml_http_post("http://127.0.0.1:8080/", JSON.stringify(message), evaluate_text_handler)
+}
+
+function evaluate_text_handler(req) {
+	console.log(req.responseText)
+	var message = JSON.parse(req.responseText)
+	console.log(message)
+	document.getElementById(message.target).innerHTML += message.content + '<br>aadasd'
+	var math = document.getElementById(message.target);
+	MathJax.Hub.Queue(["Typeset", MathJax.Hub, math]);
+}
+
+function plot_data(div_data) {
+	var message = create_message(div_data, "plot")
+	message.title = document.title
+    xml_http_post("http://127.0.0.1:8080/", JSON.stringify(message), plot_handler)
+}
+
+function plot_handler(req) {
+	var message = JSON.parse(req.responseText)
+	document.getElementById(message.title_target).innerHTML = message.title
+	// TODO: check, if the plot has been created! In not, put out a warning!
+	if(!document.getElementById('img_' + message.target)) {
+		var elem = document.createElement("img")
+		// TODO: attach right mouse click to object
+		elem.id = 'img_' + message.target
+	} else {
+		var elem = document.getElementById('img_' + message.target)
+	}
+	document.getElementById(message.target).appendChild(elem)
+	elem.src = "data:image/png;base64," + message.image_data
+	elem.style.width = "60%"
+	console.log(message.target)
+	elem.scrollTop = message.target.scrollHeight
+}
+
+function head_data(div_data) {
+	var message = create_message(div_data, "head")
+    xml_http_post("http://127.0.0.1:8080/", JSON.stringify(message), head_handler)
+}
+
+function head_handler(req) {
+	var message = JSON.parse(req.responseText)
+	var elem = document.getElementById(message.target)
+	elem.innerHTML = message.content
+	elem.scrollTop = elem.scrollHeight
+}
+
+function code_data(div_data) {
+	var message = create_message(div_data, "code")
+    xml_http_post("http://127.0.0.1:8080/", JSON.stringify(message), code_handler)
+}
+
+function code_handler(req) {
+	var message = JSON.parse(req.responseText)
+	var elem = document.getElementById(message.target)
+	elem.innerHTML = message.content
+	elem.scrollTop = elem.scrollHeight
+}
+
+function set_active(id) {
+	console.log(id)
+	active_div = id
+}
+
+function text_keypress(event) {
+	console.log(event.target.id)
+	console.log(event.which)
+	var elem = document.getElementById(event.target.id)
+	var text = elem.innerHTML
+	console.log(text)
+
+	if ((event.which || event.keyCode) == 42) {	// *
+		var elem = document.getElementById(event.target.id)
+		var text = elem.innerHTML
+		console.log(text)
+		var idx = text.indexOf('*')
+		if (idx >= 0) {
+			var new_string = text.slice(0, idx) + '<strong>' + text.slice(idx+1, -1) + '</strong>'
+			elem.innerHTML = new_string
+			return false
+		} else { return true }
+	} else {
+		return true
+	}
+}
+
+function get_divs() {
+	var message_string = ''
+	var elems = document.getElementsByTagName("*")
+	for(i=0; i < elems.length; i++) {
+		if(elems[i].getAttribute('class') == 'div_plot_main') {
+			message_string += strip_plot(get_num(elems[i]))
+		}
+		if(elems[i].getAttribute('class') == 'div_text_main') {
+			message_string += strip_text(get_num(elems[i]))
+		}		
+	}
+	return message_string
+}
+
+function strip_plot(idx) {
+	var head = document.getElementById('div_plot_header_' + idx).innerHTML
+	var title = document.getElementById('div_plot_title_' + idx).innerHTML
+	return '<plot ' + idx + '>\n<head>' + head + '</head>\n<title>' + title + '</title>\n</plot>\n'
+}
+
+function strip_text(idx) {
+	var head = document.getElementById('div_text_header_' + idx).innerHTML
+	var title = document.getElementById('div_text_body_' + idx).innerHTML
+	return '<text ' + idx + '>\n<head>' + head + '</head>\n<body>' + title + '</body>\n</text ' + idx + '>\n'
+}
+
+function save() {
+	var message = create_message('', "save")
+	message.title = document.title
+	message.content = get_divs()
+    xml_http_post("http://127.0.0.1:8080/", JSON.stringify(message), save_handler)
+}
+
+function save_handler(req) {
+	var message = JSON.parse(req.responseText)
+}
+
+function save_html() {
+	var message = create_message('', "savehtml")
+	message.title = document.title
+	console.log(document.documentElement.innerHTML)
+	message.content = document.documentElement.innerHTML
+    xml_http_post("http://127.0.0.1:8080/", JSON.stringify(message), save_handler)
+}
+
+function delete_block() {
+	var elem = active_div.parentNode
+	console.log(elem.id)
+	elem.parentNode.removeChild(elem)
+}
---
+++ test2.py
@@ -0,0 +1,191 @@
+import web
+import os
+import shutil
+import urllib
+import base64
+import simplejson
+
+from pygments import highlight
+from pygments.lexers import get_lexer_for_filename
+from pygments.formatters import HtmlFormatter
+
+# http://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
+# http://www.codinghorror.com/blog/2009/11/parsing-html-the-cthulhu-way.html
+# http://stackoverflow.com/questions/9662346/python-code-to-remove-html-tags-from-a-string
+import helper
+
+urls = ('/',  'Index')
+app = web.application(urls, globals())
+
+from pylab import *
+
+def retreive_header(message):
+	target = message['id'].replace('head_header_', 'head_body_')
+	head = message['content'].split('<br>')
+	sp = head[0].split(' ')
+	if not os.path.exists(sp[0]): 
+		return simplejson.dumps({'target' : target, 'target_pos' : message['id'], 'content' : "File doesn't exist"})
+	if len(sp) == 1: n = 10
+	# TODO: elif sp[1] == '#':	
+	else: n = int(sp[1])
+	fin = open(sp[0], 'r')	
+	if n > 0:
+		lines = []
+		it = 0
+		for line in fin:
+			lines.append(line.rstrip('\n\r'))
+			it += 1
+			if it >= n: break
+	
+	if n < 0:
+		# TODO: this is highly inefficient. 
+		# Check out http://code.activestate.com/recipes/157035-tail-f-in-python/
+		# http://stackoverflow.com/questions/136168/get-last-n-lines-of-a-file-with-python-similar-to-tail
+		lines = fin.readlines()
+		lines = lines[n:]
+	fin.close()	
+	return simplejson.dumps({'target' : target, 
+							'target_pos' : message['id'], 
+							'content' : '<br>'.join([x.rstrip('\n\r') for x in lines])})
+
+def plot_code(message):
+	target = message['id'].replace('plot_header_', 'plot_body_')
+	x = linspace(-10, 10, 100)
+	fn = message['title'] + message['id'].replace('div_plot_header_', '_') + '.png'
+	code = message['content'].replace('<p>', '\n').replace('</p>', '').replace('<br>', '\n')
+	print code
+	# TODO: check, whether the code can be executed! a 'try, except' could probably do
+	exec(code)
+	savefig(fn)
+	close()
+	with open(fn, "rb") as image_file:
+		encoded_image = base64.b64encode(image_file.read())
+	return simplejson.dumps({'target' : target, 
+							'title' : fn,
+							'title_target' :  message['id'].replace('plot_header_', 'plot_title_'), 
+							'image_data' : encoded_image})
+	
+def highlight_code(message):
+	target = message['id'].replace('code_header_', 'code_body_')
+	head = message['content'].split('<br>')
+	sp = head[0].split(' ')
+	fn = sp[0]
+	if not os.path.exists(fn): 
+		return simplejson.dumps({'target' : target, 'target_pos' : message['id'], 'content' : "File doesn't exist"})
+		
+	lexer = get_lexer_for_filename(fn)
+	fin = open(fn, 'r')
+	code = fin.read()
+	fin.close()
+	# TODO: read lines between limits, and also between tags
+	print highlight(code, lexer, HtmlFormatter())
+	return simplejson.dumps({'target' : target, 
+							'target_pos' : message['id'],
+							'content' : highlight(code, lexer, HtmlFormatter())})
+
+def texteval(message):
+	print message['content']
+	lines = message['content'].rstrip('<br>').split('<br>')
+	print lines
+	if lines[-1].find('..img::') > 0:
+		img_path = lines[-1][lines[-1].find('..img::'):].lstrip('..img::')
+		print img_path
+		return simplejson.dumps({'target' : message['id'], 
+							'success' : 'success', 
+							'content' : '<img src="%s"></img>'%img_path})
+
+	# TODO: check, whether the evaluation was successful. Use the 'success' tag for that
+	print lines[-1].replace('&nbsp;', ' ').rstrip('<br>').rstrip('=')
+	result = eval(lines[-1].replace('&nbsp;', ' ').rstrip('<br>').rstrip('='))
+	print simplejson.dumps({'target' : message['id'], 
+							'success' : 'success', 
+							'content' : result})
+	
+
+	return simplejson.dumps({'target' : message['id'], 
+							'success' : 'success', 
+							'content' : result})
+
+def save_page(message):
+	print message['content']
+	fout = open(message['title'] + '.note', 'w')
+	fout.write(message['content'].replace('<br>','\n'))
+	fout.close()
+	return  simplejson.dumps({'success' : 'success'})
+
+def save_html(message):
+	# TODO: If the source of the page has a link to an image, either on disc, or on the web, 
+	# then that has to be resolved, and the base64 representation inserted in the html file.
+	fout = open(message['title'] + '.html', 'w')
+	fout.write(message['content'])
+	fout.close()
+	return  simplejson.dumps({'success' : 'success'})
+
+class Index(object):
+	def GET(self):
+		raise web.seeother('/static/test.html')
+			
+	def POST(self):
+		message = simplejson.loads(web.data())
+		print message
+		# This could, perhaps, be simplified, if we used something like 
+		# eval(message['type'] + '_function(message)')
+		if message['type'] == 'plot':
+			return plot_code(message)
+			
+		if message['type'] == 'head':
+			return retreive_header(message)
+			
+		if message['type'] == 'code':
+			return highlight_code(message)
+			
+		if message['type'] == 'texteval':
+			return texteval(message)
+
+		if message['type'] == 'save':
+			return save_page(message)
+			
+		if message['type'] == 'savehtml':
+			return save_html(message)
+
+		else:
+			return simplejson.dumps(message)
+
+app = web.application(urls, globals())
+if __name__ == "__main__": app.run()
+
+#class StaticMiddleware:
+    #"""WSGI middleware for serving static files."""
+    #def __init__(self, app, prefix='/static/', root_path='/photo/'):
+        #self.app = app
+        #self.prefix = prefix
+        #self.root_path = root_path
+
+    #def __call__(self, environ, start_response):
+        #path = environ.get('PATH_INFO', '')
+        #path = self.normpath(path)
+
+        #if path.startswith(self.prefix):
+            #environ["PATH_INFO"] = os.path.join(self.root_path, web.lstrips(path, self.prefix))
+            #return web.httpserver.StaticApp(environ, start_response)
+        #else:
+            #return self.app(environ, start_response)
+
+    #def normpath(self, path):
+        #path2 = posixpath.normpath(urllib.unquote(path))
+        #if path.endswith("/"):
+            #path2 += "/"
+        #return path2
+
+
+#if __name__ == "__main__":
+	#wsgifunc = app.wsgifunc()
+	#wsgifunc = StaticMiddleware(wsgifunc)
+	#print wsgifunc.root_path
+	#wsgifunc = web.httpserver.LogMiddleware(wsgifunc)
+	#server = web.httpserver.WSGIServer(("0.0.0.0", 8080), wsgifunc)
+	#print "http://%s:%d/" % ("0.0.0.0", 8080)
+	#try:
+		#server.start()
+	#except KeyboardInterrupt:
+		#server.stop()
