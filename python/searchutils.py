4eb80f6a9bad4a4e60a0e0accf141630debb06e2
Zoltán Vörös <zvoros@gmail.com>
2013-04-21 00:33:10 +0200

    added searchutils.py

---
+++ python/searchutils.py
@@ -0,0 +1,28 @@
+import os
+import simplejson
+from fileutils import get_notebook_content
+
+def find_notes(dir):
+	fn = []
+	for path, subdirs, files in os.walk(dir):
+		for name in files:
+			if name.endswith('.note'): 
+				fn.append(os.path.join(path, name))
+			
+	return fn
+
+def find_string_in_file(filelist, string):
+	flist = []
+	for fn in filelist:
+		ret = inspect_file(fn, string)
+		if ret: flist.append(ret)
+	return flist
+
+def inspect_file(fn, string):
+	content = get_notebook_content(fn)
+	for element in content:
+		for cell_name in element['content']:
+			cell = element['content'][cell_name]
+			if 'props' in cell and 'searchable' in cell['props'].split(';'):
+				if cell['content'].find(string) > -1: return fn
+	return False
