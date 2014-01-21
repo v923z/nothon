d1a7b56de95c6586fad055befc6f8761448b239d
Zoltán Vörös <zvoros@gmail.com>
2014-01-21 20:10:28 +0100

    simplified arxiv_to_bibtex

--- static/js/arxiv.js
+++ static/js/arxiv.js
@@ -1,16 +1,13 @@
 function arxiv_save_entry(what) {
-	console.log(what)
+	console.log(arxiv_to_bibtex(what))
 }
 
 function arxiv_to_bibtex(what) {
 	var entry = arxiv[what]
-	var bib_str = '@article{' + entry['arxiv_id'] + ',\n'
 	var elems = new Array()
 	var keys = ['author', 'title', 'journal', 'pages', 'abstract', 'url', 'timestamp']
 	for(i in keys) {
 		elems.push('\t' + keys[i] + ' = {' + entry[keys[i]] + '}')
 	}
-	bib_str += elems.join(',\n')
-	bib_str += '\n}'
-	alert(bib_str)
+	return '@article{' + entry['arxiv_id'] + ',\n' + elems.join(',\n') + '\n}'
 }
