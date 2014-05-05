f757bf48c07d83a5ed532b1e36d2a7531fa0c797
Zoltán Vörös <zvoros@gmail.com>
2014-05-05 16:16:49 +0200

    separated notebook rendering into notebook.js

--- static/js/functions.js
+++ static/js/functions.js
@@ -252,25 +252,6 @@ $(document).ready(function () {
 		return false;
 	});
 	
-	// We do the rendering here
-	var notebook = full_notebook['notebook']
-	$('#div_title').html(full_notebook._metadata.title)
-	for(i in notebook) {
-		eval(notebook[i].type + '_render(notebook[i])')
-	}
-	
-	// TODO: this has to be done in the loop above.
-	//$(function() {
-		//$("div").each( function() {
-			//var props = $(this).data('props')
-			//if(check_tag(props, 'collapsed')) {
-				//$(this).hide()
-				//set_collapse('#' + $(this).data('main'))
-			//}
-			//if(check_tag(props, 'locked')) $(this).attr('contenteditable', false)
-		//});
-	//});
-	
 	$(function() {
 		$('.nothon_math').each( function() { $(this).html($(this).attr('alt')) })
 	});
---
+++ static/js/notebook.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {	
+	// We do the rendering here
+	var notebook = full_notebook['notebook']
+	$('#div_title').html(full_notebook._metadata.title)
+	for(i in notebook) {
+		eval(notebook[i].type + '_render(notebook[i])')
+	}
+	
+	// TODO: this has to be done in the loop above.
+	//$(function() {
+		//$("div").each( function() {
+			//var props = $(this).data('props')
+			//if(check_tag(props, 'collapsed')) {
+				//$(this).hide()
+				//set_collapse('#' + $(this).data('main'))
+			//}
+			//if(check_tag(props, 'locked')) $(this).attr('contenteditable', false)
+		//});
+	//});
+	
+})
